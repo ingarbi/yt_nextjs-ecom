8ac741ecb8adced14dbe4033f8e0056db5361947
ingarbi <104501854+ingarbi@users.noreply.github.com>
2023-05-31 17:22:12 +0300

    #6 Modal changes

--- app/(.)product/[id]/page.tsx
+++ app/(.)product/[id]/page.tsx
@@ -11,20 +11,24 @@ function Modal() {
   let [isOpen, setIsOpen] = useState(true);
   const id = useParams().id;
   const [product, setProduct] = useState<Product>();
+  const router = useRouter();
 
   useEffect(() => {
     async function fetchProduct() {
       const res = await fetch(`https://fakestoreapi.com/products/${id}`);
       const product: Product = await res.json();
-      setProduct(product)
+      setProduct(product);
     }
-    fetchProduct()
+    fetchProduct();
   }, [id]);
 
   return (
     <Dialog
       open={isOpen}
-      onClose={() => setIsOpen(false)}
+      onClose={() => {
+        setIsOpen(false);
+        router.back();
+      }}
       className="relative z-50"
     >
       {/* The backdrop, rendered as a fixed sibling to the panel container */}
@@ -37,11 +41,54 @@ function Modal() {
           {/* The actual dialog panel  */}
           <Dialog.Panel className="mx-auto max-w-3xl rounded bg-white p-10">
             <div className="flex gap-x-8 h-96">
-                {product?.image && (
-                    <div className="relative w-72 h-full hidden md:inline">
-                        <ProductImage product={product} fill />
-                    </div>
-                )}
+              {product?.image && (
+                <div className="relative w-72 h-full hidden md:inline">
+                  <ProductImage product={product} fill />
+                </div>
+              )}
+              <div className="flex flex-1 flex-col">
+                <div className="flex-1">
+                  <h4 className="font-semibold">{product?.title}</h4>
+                  <p className="font-medium text-sm">{product?.price}</p>
+                  <div className="flex items-center text-sm my-4">
+                    <p>{product?.rating.rate}</p>
+                    {product?.rating.rate && (
+                      <div className="flex items-center ml-2 mr-6">
+                        {Array.from(
+                          { length: Math.floor(product.rating.rate) },
+                          (_, i) => (
+                            <StarIcon
+                              key={i}
+                              className="h-4 w-4 text-yellow-500"
+                            />
+                          )
+                        )}
+                        {Array.from(
+                          { length: 5 - Math.floor(product.rating.rate) },
+                          (_, i) => (
+                            <StarIconOutline
+                              key={i}
+                              className="h-4 w-4 text-yellow-500"
+                            />
+                          )
+                        )}
+                      </div>
+                    )}
+                    <p className=" text-blue-600 hover:underline cursor-pointer text-xs">
+                      See all {product?.rating.count} reviews
+                    </p>
+                  </div>
+                  <p className="line-clamp-5 text-sm">{product?.description}</p>
+                </div>
+                <div className="space-y-3 text-sm">
+                  <button className="button w-full bg-blue-600 text-white border-transparent hover:border-blue-600 hover:bg-transparent hover:text-black">
+                    Add to basket
+                  </button>
+                  <button onClick={() => window.location.reload()} className="button w-full bg-blue-600 text-white border-transparent hover:border-blue-600 hover:bg-transparent hover:text-black">
+                    View full details
+                  </button>
+                </div>
+              </div>
             </div>
           </Dialog.Panel>
         </div>
